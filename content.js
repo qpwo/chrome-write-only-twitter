98112b32609a1e98a8c6052c1c43077d9578aad9
Luke Harold Miles <10591373+qpwo@users.noreply.github.com>
2024-09-14 16:48:38 -0700

    content script

---
+++ content.js
@@ -0,0 +1,36 @@
+function redirectToCompose() {
+    if (window.location.pathname !== '/compose/post') {
+        window.location.href = 'https://x.com/compose/post'
+    }
+    addModalBackground()
+}
+function addModalBackground() {
+    const modal = document.querySelector('[aria-modal="true"]')
+    if (!modal) return
+
+    let bg = document.getElementById('mycoolbg')
+    if (!bg) {
+        bg = document.createElement('div')
+        bg.id = 'mycoolbg'
+    }
+
+    // Create a new div for the background if it doesn't exist
+    // Insert the background just before the modal in the DOM
+    modal.parentNode.insertBefore(bg, modal)
+
+    // Style the background
+    bg.style.position = 'fixed'
+    bg.style.top = '0'
+    bg.style.left = '0'
+    bg.style.width = '100%'
+    bg.style.height = '100%'
+    bg.style.backgroundColor = '#222'
+    bg.style.zIndex = '9998' // Make sure it's below the modal
+    modal.style.position = 'relative'
+    modal.style.zIndex = '9999'
+}
+
+
+
+setInterval(redirectToCompose, 1000)
+setTimeout(() => setInterval(addModalBackground, 1000), 100)
