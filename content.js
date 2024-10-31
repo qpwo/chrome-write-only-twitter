9a36c5d3d2e3100f5d2395f59f508e8a2b7c1491
Luke Harold Miles <10591373+qpwo@users.noreply.github.com>
2024-10-31 06:35:21 -0700

    just block feed

--- content.js
+++ content.js
@@ -1,47 +1,8 @@
-function redirectToCompose() {
-    setTimeout(redirectToCompose, 1000)
-    if (window.location.pathname === '/i/flow/login') {
-        return
-    }
-    if (window.location.pathname !== '/compose/post') {
-        window.location.href = 'https://x.com/compose/post'
-        return
-    }
+const blocked = ['/home', '/explore']
+
+function clearcrap() {
+    setTimeout(clearcrap, 250)
+    if (!(window.location.pathname.endsWith('/home') || window.location.pathname.endsWith('/explore'))) return
+    document.querySelectorAll('article, [role="region"], [role="complementary"], [role="status"]').forEach(el => el.style.visibility = 'hidden')
 }
-function addModalBackground() {
-
-    const modal = document.querySelector('[aria-modal="true"]')
-    if (!modal) {
-        setTimeout(addModalBackground, 100) // check often before loaded
-        return
-    }
-    if (window.location.pathname !== '/compose/post') {
-        return
-    }
-    setTimeout(addModalBackground, 1000) // check less often after loaded
-
-    let bg = document.getElementById('mycoolbg')
-    if (!bg) {
-        bg = document.createElement('div')
-        bg.id = 'mycoolbg'
-    }
-
-    // Insert the background just before the modal in the DOM
-    modal.parentNode.insertBefore(bg, modal)
-
-    // Style the background
-    bg.style.position = 'fixed'
-    bg.style.top = '0'
-    bg.style.left = '0'
-    bg.style.width = '100%'
-    bg.style.height = '100%'
-    bg.style.backgroundColor = '#222'
-    bg.style.zIndex = '9998' // Make sure it's below the modal
-    modal.style.position = 'relative'
-    modal.style.zIndex = '9999'
-}
-
-
-
-redirectToCompose()
-addModalBackground()
+clearcrap()
