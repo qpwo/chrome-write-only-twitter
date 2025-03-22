d59d5bd05fa1a0cdbc7d1857ddd635fa47592e25
Luke Harold Miles <10591373+qpwo@users.noreply.github.com>
2025-03-22 03:16:06 -0700

    how it was

--- content.js
+++ content.js
@@ -1,7 +1,39 @@
 const blocked = ['/home', '/explore']
 
+
+function findMostSpecificWithText(queryText) {
+    var el = document.body
+    while (true) {
+        var found = false
+        for (var child of [...el.children]) {
+            if (child?.innerText?.includes(queryText)) {
+                el = child
+                found = true
+                break
+            }
+        }
+        if (!found) break
+    }
+    return el
+}
+
+function findBorderParent(el) {
+    while (window.getComputedStyle(el).borderRadius === '0px' && el.parentElement != null && el != el.parentElement) {
+        el = el.parentElement
+    }
+    return el
+}
+function removeSection(queryText) {
+    var el = findMostSpecificWithText(queryText)
+    if (el == null || el.style.visibility === 'hidden' || el == document.body) return
+    el = findBorderParent(el)
+    if (el == null || el.style.visibility === 'hidden' || el == document.body) return
+    el.style.visibility = 'hidden'
+}
+
 function clearcrap() {
     setTimeout(clearcrap, 250)
+    removeSection('What’s happening')
     if (!(window.location.pathname.endsWith('/home') || window.location.pathname.endsWith('/explore'))) return
     document.querySelectorAll('article, [role="region"], [role="complementary"], [role="status"]').forEach(el => el.style.visibility = 'hidden')
 }
